e7c2686318e33811627e99505f829d854b23bc32
nkomichi <nobukomichi@gmail.com>
2021-01-18 23:39:06 +0900

    imprement games

--- werewolves/game.py
+++ werewolves/game.py
@@ -24,7 +24,6 @@ def game_current(game_id):
         body = request.form['body']
         if not body:
             error = 'Body required.'
-
         if error is not None:
             flash(error)
         else:
@@ -37,43 +36,49 @@ def game_current(game_id):
             db.commit()
             return redirect(url_for('game.game_current', game_id=game_id))
 
-
     db = get_db()
+    game = get_game(game_id)
     posts = db.execute(
         'SELECT p.id, body, created, author_id, username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' ORDER BY created DESC'
     ).fetchall()
-    return render_template('game/game.html', posts=posts)
+    return render_template('game/game.html', game=game, posts=posts)
 
 
 @bp.route('/<int:game_id>/<int:day>/')
 def game_history(game_id, day):
     db = get_db()
     game = db.execute(
-        'SELECT id, name, day FROM game'
+        'SELECT id, title, day FROM game'
         ' WHERE id = ?',
         (game_id,)
     )
+    posts = db.execute(
+        'SELECT p.id, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' ORDER BY created DESC'
+    ).fetchall()
+    return render_template('game/game.html', game=game, posts=posts)
 
 @bp.route('/create', methods=('GET', 'POST'))
 @login_required
 def create():
     if request.method == 'POST':
-        name = request.form['name']
+        title = request.form['title']
         error = None
 
-        if not name:
-            error = 'Name is required.'
+        if not title:
+            error = 'Titile is required.'
 
         if error is not None:
             flash(error)
         else:
             db = get_db()
             db.execute(
-                'INSERT INTO game (name)'
+                'INSERT INTO game (title)'
                 ' VALUES (?);',
-                (name)
+                (title)
             )
             game_id = db.execute(
                 'SELECT id FROM game'
@@ -102,39 +107,21 @@ def get_post(id, check_author=True):
     return post
 
 
-@bp.route('/<int:id>/update', methods=('GET', 'POST'))
-@login_required
-def update(id):
-    post = get_post(id)
-
-    if request.method == 'POST':
-        title = request.form['title']
-        body = request.form['body']
-        error = None
-
-        if not title:
-            error = 'Title is required.'
-
-        if error is not None:
-            flash(error)
-        else:
-            db = get_db()
-            db.execute(
-                'UPDATE post SET title = ?, body = ?'
-                ' WHERE id = ?',
-                (title, body, id)
-            )
-            db.commit()
-            return redirect(url_for('game.index'))
-    
-    return render_template('game/update.html', post=post)
+def get_game(game_id):
+    """ゲーム情報を取得する
+    """
+    game = get_db().execute(
+        'SELECT id, title, day, humans, wolves FROM game WHERE id = ?', (game_id)
+    ).fetchone()
+    return game
 
 
-@bp.route('/<int:id>/delete', methods=('POST',))
-@login_required
-def delete(id):
-    get_post(id)
-    db = get_db()
-    db.execute('DELETE FROM post WHERE id = ?', (id,))
-    db.commit()
-    return redirect(url_for('game.index'))
+def is_game_finished(game):
+    """ゲームの勝敗を判定する
+    """
+    winner = None
+    if game['wolves'] <= 0:
+        winner = 'humans'
+    if game['wolves'] >= game['humans']:
+        winner = 'wolves'
+    return winner
